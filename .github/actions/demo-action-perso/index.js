afe85d7f62169655a6bcfa5fb31343366639031d
Stéphane <sthomas@campus-eni.fr>
2024-05-29 22:28:11 +0200

    Correction : suppression Dockerfile

--- .github/actions/demo-action-perso/index.js
+++ .github/actions/demo-action-perso/index.js
@@ -1,10 +1,10 @@
 ﻿const core = require('@actions/core');
 const github = require('@actions/github');
- 
+
 try {
   const myInput = core.getInput('my-input');
   console.log(`Hello ${myInput}!`);
- 
+
   core.setOutput('my-output', `Hello ${myInput}!`);
 } catch (error) {
   core.setFailed(error.message);
